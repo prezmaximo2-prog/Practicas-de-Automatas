2cdd8d0cb66fe3746681b9f7bef3600f52856dfb
Maximo.P <prezmaximo2@gmail.com>
2026-03-16 23:42:48 -0600

    Update test4.c

--- practicasLex/test4.c
+++ practicasLex/test4.c
@@ -8,7 +8,7 @@ FECHA           [0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])
 HOMOCLAVE       [A-Z0-9]{3}
 %%
 ({LETRA}{4}{FECHA}{HOMOCLAVE}) {
-    printf("\n RFC: %s\n", yytext);
+    printf("RFC: %s\n", yytext);
 }
 %%
 
